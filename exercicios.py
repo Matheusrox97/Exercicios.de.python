f57b3e55237a7508f6e8b40cf7f1d284996a4935
Matheus Ribeiro de Araujo <143028006+Matheusrox97@users.noreply.github.com>
2024-08-12 18:08:18 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,41 @@
+# Exercicio 1
+# for i in range(5):
+#     print("Hello world")
+
+
+
+
+# Exercicio 2
+# for i in range(1, 151):
+#     print(i)
+
+
+
+# Exercicio 3
+# for i in range(10, -1, -1):
+#     print(i)
+
+
+
+# Exercicio 4
+# for i in range(2, 201, 2):
+#     print(i)
+
+
+
+# Exercicio 5
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(f"{i} * {j} = {i * j}")
+#     print()
+
+
+
+# Exercicio 6
+# numero = int(input("digite um numero inteiro: "))
+# if numero < 0:
+#     print("não é inteiro positivo.")
+# else:
+#     for i in range(1, numero + 1):
+#         if i % 2 != 0:
+#             print(i)  
